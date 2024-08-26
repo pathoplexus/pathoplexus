df7a7d252773dd02d512c340e7332bb3febef45c
Emma Hodcroft <emmahodcroft@gmail.com>
2024-08-26 17:05:13 +0200

    copying monorepo folder

---
+++ monorepo/website/colors.cjs
@@ -0,0 +1,18 @@
+const mainTailwindColor = {
+    50: '#f2f9fd',
+    100: '#e4f1fa',
+    200: '#c3e3f4',
+    300: '#8eceeb',
+    400: '#52b4de',
+    500: '#2b9bcc',
+    600: '#1c7dad',
+    700: '#18638b',
+    800: '#185574',
+    900: '#194761',
+    950: '#112d40',
+    1500: '#25506e',
+};
+
+module.exports = {
+    mainTailwindColor,
+};
