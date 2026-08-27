9d4f9471eebd97fb3afc3f4950ca9167adb56373
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-08-27 14:47:25 +0200

    use pipefail (this resolves an existing issues that check_command only confirmed the last sed command succeeded)

--- scripts/db-clone/clone-prod-to-staging.sh
+++ scripts/db-clone/clone-prod-to-staging.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -Eeuo pipefail
+
 # This script orchestrates the cloning of Keycloak and Loculus databases from production to staging
 # Keycloak is dumped second and loaded first to prevent potential race conditions
 
@@ -14,27 +16,20 @@ STAGING_LOC_DB="pathoplexus_staging_loculus"
 STAGING_KC_USER="staging_keycloak_user"
 STAGING_LOC_USER="staging_loculus_user"
 
-check_command() {
-    if [ $? -ne 0 ]; then
-        echo "Error: $1"
-        exit 1
-    fi
-}
-
 # Note: Could screw up columns and values that contain `prod` etc
 # For now not an issue but might eventually want to be more surgical
 perform_sed_replacements() {
     local file="$1"
     echo "Performing sed replacements on $file..."
     # Abort the script if a line with `@` contains the word "prod" to prevent altering sensitive data
-    if grep '@' "$file" | grep -q 'prod_'; then
+    if awk '/@/ && /prod_/ { found=1; exit } END { exit !found }' "$file"; then
         echo "Error: Found 'prod' in line with '@' in $file. Aborting to prevent changing sensitive data."
         exit 1
     fi
 
     # Do not perform replacements on lines that contain the protected URL
     # see https://github.com/pathoplexus/pathoplexus/issues/1127
-    local protected_url='"https://pathoplexus.org/about/governance/minutes/2026-06-01_EB_Resolutions.pdf"'
+    local protected_url='https://pathoplexus.org/about/governance/minutes/2026-06-01_EB_Resolutions.pdf'
     local placeholder='__PROTECTED_PATHOPLEXUS_URL__'
     sed -i "s#${protected_url}#${placeholder}#g" "$file"
 
@@ -45,29 +40,22 @@ perform_sed_replacements() {
 
     # Restore the protected string
     sed -i "s#${placeholder}#${protected_url}#g" "$file"
-
-    check_command "Failed to perform sed replacements on $file"
 }
 
 echo "Dumping production Loculus database..."
 $CHILD_SCRIPT dump $PROD_LOC_DB $PROD_LOC_DUMP
-check_command "Failed to dump production Loculus database"
 
 echo "Dumping production Keycloak database..."
 $CHILD_SCRIPT dump $PROD_KC_DB $PROD_KC_DUMP
-check_command "Failed to dump production Keycloak database"
-
 perform_sed_replacements $PROD_KC_DUMP
 
 perform_sed_replacements $PROD_LOC_DUMP
 
 echo "Loading Keycloak dump to staging..."
 $CHILD_SCRIPT load $STAGING_KC_DB $PROD_KC_DUMP $STAGING_KC_USER
-check_command "Failed to load Keycloak dump to staging"
 
 echo "Loading Loculus dump to staging..."
 $CHILD_SCRIPT load $STAGING_LOC_DB $PROD_LOC_DUMP $STAGING_LOC_USER
-check_command "Failed to load Loculus dump to staging"
 
 echo "Cloning process completed successfully!"
 echo "Please restart the backend to apply changes."
